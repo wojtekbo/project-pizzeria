c5fc5d5c850521091ffe51bcbaad7d72f6decafe
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-01-17 22:03:17 +0100

    Add initAmountWidget in booking tab

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,10 +1,19 @@
-import {templates} from '../settings.js';
+import {templates, select, settings} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 
 class Booking {
   constructor(element) {
     const thisBooking = this;
     thisBooking.render(element);
+    thisBooking.getElements();
+    thisBooking.initAmountWidget();
+    // thisBooking.initPeopleAmountWidget();
     // thisBooking.initWidgets();
+  }
+  getElements() {
+    const thisBooking = this;
+    thisBooking.dom.peopleAmountElem = document.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmountElem = document.querySelector(select.booking.hoursAmount);
   }
   render(element) {
     const thisBooking = this;
@@ -12,6 +21,13 @@ class Booking {
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = bookingHTML;
+  }
+  initAmountWidget() {
+    const thisBooking = this;
+    thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmountElem, settings.amountWidget.defaultValue);
+    thisBooking.dom.peopleAmountElem.addEventListener('updated', () => console.log('test 1'));
+    thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmountElem, settings.amountWidget.defaultValue);
+    thisBooking.dom.hoursAmountElem.addEventListener('updated', () => console.log('test 2'));
   }
 }
 

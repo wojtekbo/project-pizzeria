117591e90d1272986ccba8a7f27f846fb1b4d632
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-01-22 20:28:11 +0100

    Add fetch data

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
 import {templates, select, settings} from '../settings.js';
+import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -11,9 +12,43 @@ class Booking {
     thisBooking.initAmountWidget();
     thisBooking.initDatePicker();
     thisBooking.initHourPicker();
+    thisBooking.getData();
     // thisBooking.initPeopleAmountWidget();
     // thisBooking.initWidgets();
   }
+  getData() {
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+    const params = {
+      booking: [startDateParam, endDateParam],
+      eventsCurrent: [settings.db.notRepeatParam, startDateParam, endDateParam],
+      eventsRepeat: [settings.db.repeatParam, endDateParam],
+    };
+
+    // console.log('params', params);
+
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.bookings + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.events + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat: settings.db.url + '/' + settings.db.events + '?' + params.eventsRepeat.join('&'),
+    };
+
+    // console.log(urls);
+
+    Promise.all([fetch(urls.booking), fetch(urls.eventsCurrent), fetch(urls.eventsRepeat)])
+      .then(function (allResponses) {
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([bookingsResponse.json(), eventsCurrentResponse.json(), eventsRepeatResponse.json()]);
+      })
+      .then(function ([bookings, eventsCurrentResponse, eventsRepeatResponse]) {
+        console.log(bookings, eventsCurrentResponse, eventsRepeatResponse);
+      });
+  }
+
   getElements() {
     const thisBooking = this;
     thisBooking.dom.peopleAmountElem = document.querySelector(select.booking.peopleAmount);
